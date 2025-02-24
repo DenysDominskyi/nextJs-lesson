e56ceeefc250ae5eb2a20f825067f02818f80c33
DenysDominskyi <denysdominskyi@gmail.com>
2025-02-24 12:40:49 +0100

    first commit

---
+++ pages/characters/index.tsx
@@ -0,0 +1,33 @@
+import { API } from "../../assets/api/api";
+import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api";
+import { Header } from "../../components/Header/Header";
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
+
+export const getStaticProps = async () => {
+  const characters = await API.rickAndMorty.getCharacters();
+
+  return {
+    props: {
+      characters,
+    },
+  };
+};
+
+type PropsType = {
+  characters: ResponseType<CharacterType>;
+};
+
+const Characters = (props: PropsType) => {
+  const { characters } = props;
+  const charactersList = characters.results.map(ch => (
+    <div key={ch.id}>{ch.name}</div>
+  ))
+  return (
+    <PageWrapper>
+      <Header />
+      {charactersList}
+    </PageWrapper>
+  );
+};
+
+export default Characters;
